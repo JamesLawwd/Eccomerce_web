753d4d64a83fecb7da3e5925f14189e039a6cf6e
james <jleo56291@gmail.com>
2024-06-05 11:50:21 +0300

    slider page

--- src/components/CartModal.tsx
+++ src/components/CartModal.tsx
@@ -6,13 +6,16 @@ const CartModal = () => {
 
     const cartItems = false
   return (
-    <div className='absolute p-4 rounded-md shadow-[0_3px_10px_rgb(0,0,0.2)] bg-white top-12 right-0 flex flex-col gap-6 z-20'>
+    <div className='w-max absolute p-4 rounded-md shadow-[0_3px_10px_rgb(0,0,0.2)] bg-white top-12 right-0 flex flex-col gap-6 z-20'>
       {!cartItems ? (
         <div className=''>Cart is Empty</div>
       ) : (
-        <div className=''>
-            <Image src="" alt='' width={72} height={96} className='object-cover rounded-md'/>
-            <div className=''>
+        <>
+         <h2 className="text-xl">Shopping Cart</h2>
+          {/* LIST */}
+        <div className='flex gap-4'>
+            <Image src="https://images.pexels.com/photos/934070/pexels-photo-934070.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt='' width={72} height={96} className='object-cover rounded-md'/>
+            <div className='flex flex-col justify-between w-full'>
                 {/* TOP */}
                 <div className=""></div>
             
@@ -29,6 +32,7 @@ const CartModal = () => {
 
             </div>
         </div>
+        </>
       )}
     </div>
   )
